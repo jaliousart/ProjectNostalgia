8bef188b9612700d548e172d76528fd03d7707b4
Liam Nelski <ljnelski@gmail.com>
2020-05-11 23:52:22 -0400

    Continued with programming character movement
    [Add]  
    - Enum of all the different character states
    [Modify]
    - Player movement script

--- Project Nostaligia/Assets/Scripts/MovementController.cs
+++ Project Nostaligia/Assets/Scripts/MovementController.cs
@@ -1,15 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Assets.Scripts;
 
 public class MovementController : MonoBehaviour
 {
     public CharacterInput characterInput;
     public float jumpForce;
     public float moveForce;
+    public float maxSpeed;
 
-    private Rigidbody2D rbody;
+    private Rigidbody2D rbody;    
     private bool isGrounded;
+
+    public CharacterState CharacterState { private set; get; }
     
     // Update is called once per frame
     private void Start()
@@ -20,16 +24,47 @@ public class MovementController : MonoBehaviour
     void FixedUpdate()
     {
         Move();
-    }   
+    }
     private void Move()
     {
-        if (characterInput.Jump && isGrounded)
-        {            
-            AddForce(new Vector2(0f, jumpForce));
-            isGrounded = false;
-            Debug.Log("Character jumped");
+        switch (CharacterState)
+        {
+            case CharacterState.Idle:
+                break;
+            case CharacterState.Walk:
+                break;
+            case CharacterState.Jump:
+                break;
+            case CharacterState.Fall:
+                break;
+            case CharacterState.Cling:
+                break;
+            case CharacterState.Crouch:
+                break;
+            case CharacterState.Crawl:
+                break;
+            case CharacterState.Slide:
+                break;
+            default:
+                break;
         }
-        AddForce(new Vector2(moveForce * characterInput.Direction, 20f));
+        //if (characterInput.Jump && isGrounded)
+        //{
+        //    AddForce(new Vector2(0f, jumpForce));
+        //    isGrounded = false;
+        //    Debug.Log("Character jumped");
+        //}
+        //if (isGrounded)
+        //{
+        //    if (rbody.velocity.magnitude <= maxSpeed)
+        //    {
+        //        AddForce(new Vector2(moveForce * characterInput.Direction, 0f));
+        //    }
+        //    else if (characterInput.Crouch)
+        //    {
+
+        //    }
+        //}
     }
     private void AddForce(Vector2 direction)
     {
